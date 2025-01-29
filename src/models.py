9e5e9a3986f90d70edbf7ead1335c22cd2f5eeaa
PilarGallegoS <pgsubias@gmail.com>
2025-01-29 19:03:34 +0000

    actualizacion con mis datos de models y admin

--- src/models.py
+++ src/models.py
@@ -1,12 +1,50 @@
 from flask_sqlalchemy import SQLAlchemy
+from dataclasses import dataclass
+from flask import Flask
+from sqlalchemy import ForeignKey
 
 db = SQLAlchemy()
 
+@dataclass
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    __tablename__ = 'user'
+    id:int = db.Column(primary_key=True, nullable=False)
+
+@dataclass
+class Favorites(db.Model):
+    __tablename__ = 'favorites'
+    id:int = db.Column( primary_key=True)
+    external_id:str = db.Column(db.String(50))
+    name:str = db.Column(db.String(50))
+    user_id:int = db.Column(db.Integer, ForeignKey('user.id'), nullable=False)
+    type = db.Column(db.Enum('Species', 'Planets', 'People'))
+
+@dataclass
+class Species(db.Model):
+    __tablename__ = 'species'
+    uid:int = db.Column(db.Integer, primary_key=True)
+    description:str = db.Column(db.String(250))
+    name:str = db.Column(db.String(50))
+    homeworld:int = db.Column(db.Integer, ForeignKey('planets.uid'), nullable=False)
+    classification:str = db.Column(db.String(50))
+
+@dataclass
+class Planets(db.Model):
+    __tablename__ = 'planets'
+    uid:int = db.Column(db.Integer, primary_key=True)
+    name:str = db.Column(db.String(50))
+    gravity:str = db.Column(db.String(250))
+
+@dataclass
+class People(db.Model):
+    __tablename__ = 'people'
+    uid:int = db.Column(db.Integer, primary_key=True)
+    description:str = db.Column(db.String(250))
+    name:str = db.Column(db.String(50))
+    homeworld:int = db.Column(db.Integer, ForeignKey('planets.uid'), nullable=False)
+
+
+
 
     def __repr__(self):
         return '<User %r>' % self.username
