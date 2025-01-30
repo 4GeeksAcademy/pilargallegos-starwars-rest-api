6d863a6b86c26e7cfdfaef16421d1371c385284d
PilarGallegoS <pgsubias@gmail.com>
2025-01-30 15:46:02 +0000

    arreglo de codigo en models y admin

--- src/admin.py
+++ src/admin.py
@@ -3,6 +3,12 @@ from flask_admin import Admin
 from models import db, User, Favorites, Species, Planets, People
 from flask_admin.contrib.sqla import ModelView
 
+class AdminView(ModelView):
+    def __init__(self, model, *args, **kwargs):
+        self.column_list = [c.key for c in model.__table__.columns]
+        self.form_columns = self.column_list
+        super(AdminView, self).__init__(model, *args, **kwargs)
+
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
     app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
@@ -11,10 +17,10 @@ def setup_admin(app):
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
-    admin.add_view(ModelView(Favorites, db.session))
-    admin.add_view(ModelView(Species, db.session))
+    admin.add_view(AdminView(Favorites, db.session))
+    admin.add_view(AdminView(Species, db.session))
     admin.add_view(ModelView(Planets, db.session))
-    admin.add_view(ModelView(People, db.session))
+    admin.add_view(AdminView(People, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/models.py
+++ src/models.py
@@ -1,57 +1,70 @@
-from flask_sqlalchemy import SQLAlchemy
 from dataclasses import dataclass
-from flask import Flask
 from sqlalchemy import ForeignKey
-
+from flask_sqlalchemy import SQLAlchemy
+import enum
 db = SQLAlchemy()
-
 @dataclass
 class User(db.Model):
     __tablename__ = 'user'
-    id:int = db.Column(primary_key=True, nullable=False)
+    id:int = db.Column(db.Integer, primary_key=True, index=True, nullable=False)
+    name:str = db.Column(db.String(30), nullable=False)
+    password:str = db.Column(db.String(30), nullable=False, unique=True)
+class FavoritesType(enum.Enum):
+    SPECIES = "SPECIES"
+    PLANETS = "PLANETS"
+    PEOPLE = "PEOPLE"
 
 @dataclass
 class Favorites(db.Model):
     __tablename__ = 'favorites'
-    id:int = db.Column( primary_key=True)
-    external_id:str = db.Column(db.String(50))
-    name:str = db.Column(db.String(50))
-    user_id:int = db.Column(db.Integer, ForeignKey('user.id'), nullable=False)
-    type = db.Column(db.Enum('Species', 'Planets', 'People'))
+    id:int = db.Column(db.Integer, primary_key=True, index=True, nullable=False)
+    user_id:int = db.Column(db.Integer, ForeignKey('user.id'),index=True, nullable=False)
+    external_id:int = db.Column(db.Integer, nullable=False)
+    type:str = db.Column(db.Enum(FavoritesType), nullable=False)
+    name:str = db.Column(db.String(30), nullable=False)
 
 @dataclass
 class Species(db.Model):
     __tablename__ = 'species'
-    uid:int = db.Column(db.Integer, primary_key=True)
-    description:str = db.Column(db.String(250))
-    name:str = db.Column(db.String(50))
+    uid:int = db.Column(db.Integer, primary_key=True, unique=True, index=True, nullable=False)
+    description:str = db.Column(db.String(50), nullable=False)
+    name:str = db.Column(db.String(30), nullable=False)
     homeworld:int = db.Column(db.Integer, ForeignKey('planets.uid'), nullable=False)
-    classification:str = db.Column(db.String(50))
 
 @dataclass
 class Planets(db.Model):
     __tablename__ = 'planets'
-    uid:int = db.Column(db.Integer, primary_key=True)
-    name:str = db.Column(db.String(50))
-    gravity:str = db.Column(db.String(250))
+    uid:int = db.Column(db.Integer, primary_key=True, unique=True, index=True, nullable=False)
+    name:str = db.Column(db.String(30), nullable=False)
+    gravity:str = db.Column(db.String(100), nullable=False)
 
 @dataclass
 class People(db.Model):
     __tablename__ = 'people'
-    uid:int = db.Column(db.Integer, primary_key=True)
-    description:str = db.Column(db.String(250))
-    name:str = db.Column(db.String(50))
+    uid:int = db.Column(db.Integer, primary_key=True, unique=True, index=True, nullable=False)
+    description:str = db.Column(db.String(50), nullable=False)
+    name:str = db.Column(db.String(30), nullable=False)
     homeworld:int = db.Column(db.Integer, ForeignKey('planets.uid'), nullable=False)
 
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#     password = db.Column(db.String(80), unique=False, nullable=False)
+#     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+#     def __repr__(self):
+#         return '<User %r>' % self.username
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "email": self.email,
+#             # do not serialize the password, its a security breach
+#         }
 
 
 
-    def __repr__(self):
-        return '<User %r>' % self.username
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+
+
+
+
+
